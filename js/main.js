cfaa3973aeea76b1c7f56f6c7543d9d49f45e88a
Alvaro Carrascosa <varocarras@gmail.com>
2021-05-02 21:59:41 -0400

    LinkedIn

--- js/main.js
+++ js/main.js
@@ -67,12 +67,11 @@ var files = (function () {
         }
     };
     Singleton.defaultOptions = {
-        "about.txt": "This website was made using only pure JavaScript with no extra libraries.\nI made it dynamic so anyone can use it, just download it from GitHub and change the config text according to your needs.\nIf you manage to find any bugs or security issues feel free to email me: luisbraganca@protonmail.com",
-        "getting_started.txt": "First, go to js/main.js and replace all the text on both singleton vars.\n- configs: All the text used on the website.\n- files: All the fake files used on the website. These files are also used to be listed on the sidenav.\nAlso please notice if a file content is a raw URL, when clicked/concatenated it will be opened on a new tab.\nDon't forget also to:\n- Change the page title on the index.html file\n- Change the website color on the css/main.css\n- Change the images located at the img folder. The suggested sizes are 150x150 for the avatar and 32x32/16x16 for the favicon.",
-        "contact.txt": "mail@example.com",
-        "social_network_1.txt": "https://www.socialite.com/username/",
-        "social_network_2.txt": "https://example.com/profile/9382/",
-        "test_file.txt": "https://pdfhost.io/edit?doc=0f4b3619-002d-484a-ab1d-dd4194d06dca"
+        "resume.pdf": "https://drive.google.com/file/d/1JQ2dY3Qiz9DZ924zUwE4AqSFz98ooarb/view?usp=sharing",
+        "linkedin.com": "https://www.linkedin.com/in/varoc",
+        //"getting_started.txt": "First, go to js/main.js and replace all the text on both singleton vars.\n- configs: All the text used on the website.\n- files: All the fake files used on the website. These files are also used to be listed on the sidenav.\nAlso please notice if a file content is a raw URL, when clicked/concatenated it will be opened on a new tab.\nDon't forget also to:\n- Change the page title on the index.html file\n- Change the website color on the css/main.css\n- Change the images located at the img folder. The suggested sizes are 150x150 for the avatar and 32x32/16x16 for the favicon.",
+        "contact.txt": "contact@alvaro.life",
+        "about.txt": "This website was made for personal and professional use\nIt was made purely in Javascript with no extra libraries \nIf you manage to find any bugs or security issues feel free to email me: contact@alvaro.life"
     };
     return {
         getInstance: function (options) {
